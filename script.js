bedc76fea920d65fb444c0d7cafdd98cf789532b
Ana Sander <anagsander@gmail.com>
2024-01-29 19:13:00 -0300

    search-js

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ const searchInput = document.getElementById("search-input");
 const searchResult = document.getElementById("search-result");
 const searchPlaylist = document.getElementById("result-playlist");
 
+searchResult.classList.add("hidden");
+
 function requestApi(searchTerm) {
   const url = `http://localhost:3000/ost?name_like=${searchTerm}`;
-  fetch(url).then((response) => response.json());
-  // .then((result) => displayResult(result));
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => displayResult(result));
+}
+
+function displayResult(result) {
+  searchPlaylist.classList.add("hidden");
+  const ostName = document.getElementById("ost-name");
+  const ostImg = document.getElementById("ost-img");
+  const ostComposer = document.getElementById("ost-composer");
+
+  result.forEach((element) => {
+    ostName.innerText = element.name;
+    ostImg.src = element.img;
+    ostComposer.innerText = element.composer;
+  });
+
+  searchResult.classList.remove("hidden");
 }
 
 document.addEventListener("input", () => {
